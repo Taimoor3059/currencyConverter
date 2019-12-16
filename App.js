03d12de3c556df0b7c4066ddf26c26aa8fdf456c
TaimoorTahir <taimoorhtahir@gmail.com>
2019-12-16 00:11:54 -0600

    creating the State and the inputValue

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <TouchableOpacity>
-        <Text style={styles.submit}>Submit</Text>
-      </TouchableOpacity>
-    </View>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue : ""
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          placeholder = "Amount to be converted"
+          keyboardType = "numeric"
+          value = {this.state.inputValue}
+          selectionColor = "#FFF"
+          onChangeText = { (inputValue) => {this.setState({inputValue})} } 
+        />
+  
+        <TouchableOpacity>
+          <Text style={styles.submit}>Submit</Text>
+        </TouchableOpacity>
+  
+      </View>
+    );
+  }
+  
 }
 
 const styles = StyleSheet.create({
